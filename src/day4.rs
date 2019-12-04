8546cefaeacd5a2f8fe4553a0d63adbe6955aa4e
Alluet <alluet@alluet.com>
2019-12-04 01:32:18 -0500

    Refactor Day 4

--- src/day4.rs
+++ src/day4.rs
@@ -1,15 +1,13 @@
 use std::iter;
+use std::ops::RangeInclusive;
 use aoc_runner_derive::aoc;
 
-fn parse_input(input: &str) -> [u32; 2] {
+fn parse_input(input: &str) -> RangeInclusive<u32> {
     let mut iter = input
         .split("-")
         .map(|n| n.parse::<u32>().unwrap());
     
-    [
-        iter.next().unwrap(),
-        iter.next().unwrap(),
-    ]
+    iter.next().unwrap() ..= iter.next().unwrap()
 }
 
 fn get_digits(n: u32) -> [u8; 6] {
@@ -36,46 +34,38 @@ fn is_ordered(digits: [u8; 6]) -> bool {
 }
 
 #[aoc(day4, part1)]
-fn part1(input: &str) -> u32 {
-    let [lower, upper] = parse_input(input);
-
-    let mut matches = 0;
-    for n in lower..=upper {
-        let digits = get_digits(n);
-        if is_ordered(digits) {
-            if digits.windows(2)
-                .filter(|window| window[0] == window[1])
-                .next().is_some()
-            {
-                matches += 1;
+fn part1(input: &str) -> usize {
+    parse_input(input)
+        .filter_map(|n| {
+            let digits = get_digits(n);
+            if is_ordered(digits) {
+                digits.windows(2)
+                    .filter(|window| window[0] == window[1])
+                    .next().map(|_| ())
+            } else {
+                None
             }
-        }
-    }
-
-    matches
+        })
+        .count()
 }
 
 #[aoc(day4, part2)]
-fn part2(input: &str) -> u32 {
-    let [lower, upper] = parse_input(input);
-
-    let mut matches = 0;
-    for n in lower..=upper {
-        let digits = get_digits(n);
-
-        if is_ordered(digits) {
-            let mut counts = [0u8; 10];
-            for &digit in digits.iter() {
-                counts[digit as usize] += 1;
-            }
-            for &count in counts.iter() {
-                if count == 2 {
-                    matches += 1;
-                    break;
+fn part2(input: &str) -> usize {
+    parse_input(input)
+        .filter_map(|n| {
+            let digits = get_digits(n);
+            if is_ordered(digits) {
+                let mut counts = [0u8; 10];
+                for &digit in digits.iter() {
+                    counts[digit as usize] += 1;
+                }
+                for &count in counts.iter() {
+                    if count == 2 {
+                        return Some(());
+                    }
                 }
             }
-        }
-    }
-
-    matches
+            None
+        })
+        .count()
 }
